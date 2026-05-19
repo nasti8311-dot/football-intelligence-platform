0d8735c87205931cf4c56e025435f080c3d085dc
Your Name <your@email.com>
2026-05-19 17:50:03 +0200

    Enable trusted host for auth

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import { prisma } from "@/lib/prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  trustHost: true,
   session: { strategy: "jwt" },
   providers: [
     Google({
