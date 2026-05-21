87a2d237cc9e79783140ab6026861249dfed5aab
Your Name <your@email.com>
2026-05-21 22:00:52 +0200

    Add league key mapping

---
+++ lib/league-key-map.ts
@@ -0,0 +1,27 @@
+export const LEAGUE_KEY_MAP: Record<string, string> = {
+  "Premier League": "soccer_epl",
+  "EPL": "soccer_epl",
+
+  "Primera Division": "soccer_spain_la_liga",
+  "La Liga": "soccer_spain_la_liga",
+
+  "Serie A": "soccer_italy_serie_a",
+
+  "Bundesliga": "soccer_germany_bundesliga",
+
+  "Ligue 1": "soccer_france_ligue_one",
+
+  "Eredivisie": "soccer_netherlands_eredivisie",
+
+  "Belgium First Div": "soccer_belgium_first_div",
+
+  "MLS": "soccer_usa_mls",
+
+  "UEFA Champions League": "soccer_uefa_champs_league",
+
+  "UEFA Europa Conference League":
+    "soccer_uefa_europa_conference_league",
+
+  "Austrian Football Bundesliga":
+    "soccer_austria_bundesliga",
+};
